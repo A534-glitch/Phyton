56d5b0e41a13fd744a9cafcd1fee485edb451b18
A534-glitch <sadharsh306@gmail.com>
2024-11-27 19:28:26 +0530

    Add files via upload

---
+++ areaofrec.py
@@ -0,0 +1,20 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+
+
+if __name__ == "__main__":
+    
+    length = float(input("Enter the length of the rectangle: "))
+    breadth = float(input("Enter the breadth of the rectangle: "))
+
+    
+    rect = Rectangle(length, breadth)
+
+    
+    print(f"The area of the rectangle is: {rect.area()}")
