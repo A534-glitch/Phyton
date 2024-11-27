ea121b4d8adc3486834c5328d7eb68c990874975
A534-glitch <sadharsh306@gmail.com>
2024-11-27 19:38:01 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,6 @@ print("Ascending order is:", l)
 l = list(dict1.items())
 l.sort(reverse=True)
 print("Descending order is :", l)
-
 dict2 = {4: "plum", 5: "cherry"}
 print(f"Dictionary 1: {dict2}")
 dict1.update(dict2)
