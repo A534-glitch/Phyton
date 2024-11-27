9674acb056b9c66e15e301d3fedb676d8d69b5d7
A534-glitch <sadharsh306@gmail.com>
2024-11-27 19:38:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,13 @@
 
 def add(num1, num2):
     return num1 + num2
-
-
 def subtract(num1, num2):
     return num1 - num2
 
 def multiply(num1, num2):
     return num1 * num2
-
 def divide(num1, num2):
     return num1 / num2
-
 print("Please select operation -\n" \
         "1. Add\n" \
         "2. Subtract\n" \
