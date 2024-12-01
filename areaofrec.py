aa620d659c6290527169231477b3067288c649e6
A534-glitch <sadharsh306@gmail.com>
2024-12-01 14:16:08 +0530

    Update areaofrec.py

--- areaofrec.py
+++ areaofrec.py
@@ -1,20 +1,11 @@
 class Rectangle:
-    def __init__(self, length, breadth):
-        self.length = length
-        self.breadth = breadth
-
-    def area(self):
-        return self.length * self.breadth
-
-
-
-if __name__ == "__main__":
-    
-    length = float(input("Enter the length of the rectangle: "))
-    breadth = float(input("Enter the breadth of the rectangle: "))
-
-    
-    rect = Rectangle(length, breadth)
-
-    
-    print(f"The area of the rectangle is: {rect.area()}")
+def __init__(self, length, breadth):
+self.length = length
+self.breadth = breadth
+def area(self):
+return self.length * self.breadth
+if __name__ == "__main__":    
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+rect = Rectangle(length, breadth)
+print(f"The area of the rectangle is: {rect.area()}")
