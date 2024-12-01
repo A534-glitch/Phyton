7667073e50a03738dbe37a399de88cc1dec10685
A534-glitch <sadharsh306@gmail.com>
2024-12-01 14:06:33 +0530

    Add files via upload

---
+++ G1.py
@@ -0,0 +1,24 @@
+def arear(l,b):
+a=l*b
+print("Area of Rectangle is:",a)
+def perir(l,b):
+p=2*(l+b)
+print("Perimeter of Rectangle is:",p)
+def areac(r):
+a = 3.14* r**2
+print("Area of Circle is:", a)
+def peric(r):
+p = 2 * 3.14 * r
+print("Perimeter of a Circle is:", p)
+def areacub(l,b,h):
+a = 2*((l*b) + (b*h) + (h*l))
+print("Area of Cuboid is:", a)
+def pericub(l,b,h):
+p = 4*(l+b+h)
+print("Perimeter of Cuboid is:", p)
+def areas(r):
+a = 4*3.14*r*r
+print("Area of Sphere is:", a)
+def peris(r):
+p = 6.2832 * r
+print("Perimeter of Sphere is:", p)
