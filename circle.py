c55fd588b7930fb6bfcbda76ae3c5b3ac89fd332
A534-glitch <sadharsh306@gmail.com>
2024-12-03 18:45:56 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,8 @@
+def areac(r):
+    a = 3.14*r**2
+    print("Area of circle is:", a)
+
+
+def peric(r):
+    p = 2 *3.14*r
+    print("perimeter of circle is:", p)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+def arear(l,b):
+    a=l*b
+    print("Area of rectanglr is:",a)
+def perir(l,b):
+    p=2*(l+b)
+    print("perimeter of rectangle is:",p)
