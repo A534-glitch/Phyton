c17a6636c90124a81536169a82e15792e2cf13cc
A534-glitch <sadharsh306@gmail.com>
2024-12-25 12:19:53 +0530

    Update sumoflist.py

--- sumoflist.py
+++ sumoflist.py
@@ -4,14 +4,11 @@ print("fibonacci series:")
 for i in range(n):
     print(a)
     a,b=b,a+b
-
-
 num= int(input("enter the number:"))
 factorial=1
 for i in range(1,num+1):
     factorial*=i
 print("factorial of",num,"is",factorial)
-
 numbers = [1, 2, 3, 4, 5]
 total_sum = sum(numbers)
 print("The sum of all items in the list is:", total_sum)
